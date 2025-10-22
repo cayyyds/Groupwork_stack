903e844fe214dd52f04cdb91800e1469fc731128
cayyyds <122010571+cayyyds@users.noreply.github.com>
2025-10-22 18:11:55 +0800

    isp and osp

--- main.cpp
+++ main.cpp
@@ -13,5 +13,36 @@ int main() {
     LStack<double> OPEN;
     LStack<char> OPTR;
 
+    char ch;
+   
+
     
 }
+int isp(char op){
+    switch(op){
+        case '=':return 0;
+        case '+':return 3;
+        case '-':return 3;
+        case '*':return 5;
+        case '/':return 5;
+        case '%':return 5;
+        case '(':return 1;
+        case ')':return 8;
+        case '^':return 7;
+        case '&':return 7;
+    }
+}//给出各个运算符的栈内优先级
+int osp(char op){
+    switch(op){
+        case '=':return 0;
+        case '+':return 2;
+        case '-':return 2;
+        case '*':return 4;
+        case '/':return 4;
+        case '%':return 4;
+        case '(':return 8;
+        case ')':return 1;
+        case '^':return 6;
+        case '&':return 6;
+    }
+}//给出各个运算符的栈外优先级
