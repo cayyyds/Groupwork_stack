13a5028137b90bd2838f0b2ef6de14798a014df6
Shergould <1471427284@qq.com>
2025-10-23 00:38:08 +0800

    update cal function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 #include"Stack.h"
 using namespace std;
 int isp(char op);
@@ -8,7 +9,7 @@ void GetNextChat(char &ch);
 bool isdigit(char ch);
 bool isoperator(char ch);
 
-//盛佳一编写Get2Operands函数
+// 盛佳一编写Get2Operands函数
 bool Get2Operands(LStack<double> &OPEN,double &x,double &y){
     if (OPEN.length() < 2)
         return false; //若Stack的长度小于2，返回flase
@@ -19,15 +20,51 @@ bool Get2Operands(LStack<double> &OPEN,double &x,double &y){
     }
 }
 
+// LiuYisen编写的cal函数
+bool cal(char op, double x, double y, double& r) {
+    switch (op) {
+        case '+': // 加法
+            r = x + y; 
+            return true;
+        case '-': // 减法
+            r = x - y; 
+            return true;
+        case '*': // 乘法
+            r = x * y; 
+            return true;
+		case '/': // 除法
+            if (y == 0) {
+				cerr << "ERROR被除数不能为0" << endl;
+				return false; // 除0错误
+            }
+            r = x / y; 
+            return true;
+        case '%': // 取模
+            // 对于 double 使用 fmod
+            r = fmod(x, y);
+            return true;
+        case '^': // 乘方
+			r = pow(x, y);
+			return true;
+        case '&': // 开方
+            if (y == 0) {
+                cerr << "ERROR:y不能为0" << endl;
+				return false; // 开0次方无意义
+            }
+			r = pow(x, 1.0 / y);
+            return true;
+        default:
+            cerr << "ERROR:这是一个未知操作符" << op << endl;
+            return false;
+	}
+}
+
 int main() {
     LStack<double> OPEN;
     LStack<char> OPTR;
-
-    char ch;
-   
-
     
 }
+
 int isp(char op){
     switch(op){
         case '=':return 0;
@@ -41,7 +78,7 @@ int isp(char op){
         case '^':return 7;
         case '&':return 7;
     }
-}//给出各个运算符的栈内优先级
+} //给出各个运算符的栈内优先级
 int osp(char op){
     switch(op){
         case '=':return 0;
@@ -55,7 +92,7 @@ int osp(char op){
         case '^':return 6;
         case '&':return 6;
     }
-}//给出各个运算符的栈外优先级
+} //给出各个运算符的栈外优先级
 
 bool isdigit(char ch) {
     return ch >= '0' && ch <= '9';
