151fb3d8f31c68617d5e5962df535c9dbee7c34d
lorntz <lorntz273@gmail.com>
2025-10-22 18:16:22 +0800

    finished Get2Operands function

--- main.cpp
+++ main.cpp
@@ -7,11 +7,21 @@ bool cal(char op,double x,double y,double &r);
 void GetNextChat(char &ch);
 bool isdigit(char ch);
 bool isoperator(char ch);
-bool Get2Operands(LStack<double> &OPEN,double &x,double &y);
+
+//盛佳一编写Get2Operands函数
+bool Get2Operands(LStack<double> &OPEN,double &x,double &y){
+    if (OPEN.length() < 2)
+        return false; //若Stack的长度小于2，返回flase
+    else{
+        x = OPEN.pop();
+        y = OPEN.pop();
+        return true;
+    }
+}
 
 int main() {
     LStack<double> OPEN;
     LStack<char> OPTR;
 
-    
 }
+
